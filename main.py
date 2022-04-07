2ee02fcae1487a077bef21b4280f093218551906
GrunCrow <albamrqz751@gmail.com>
2022-04-07 19:18:52 +0200

    Fixed some errors

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, WHITE, BLACK
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, WHITE, BLACK, GREY
 from checkers.game import Game
-from minimax.algorithm import minimax
+from minimax.algorithm import minimax, alpha_beta_pruning
 import time
 
 FPS = 60
 
-depth = 3
+ALPHA = float('-inf')
+BETA = float('inf')
+
+depth = 4
 
 evaluate_function = 2
-# 1 = evaluate_function_1 : without kings
-# 2 = evaluate_function_2: with kings + heuristics
+# 1 = evaluate_function_1 : depending on number of pawns and kings left
+# 2 = evaluate_function_2: function_1 + depending on the side of the board
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
@@ -35,7 +38,13 @@ def main():
 
         if game.turn == WHITE:
             white_start_time = time.time()
-            value, new_board = minimax(game.get_board(), depth, True, game, 1, WHITE)
+
+            # Minimax
+            # value, new_board = minimax(game.get_board(), depth, True, game, 1, WHITE)
+
+            # Alpha-Beta Pruning
+            value, new_board = alpha_beta_pruning(game.get_board(), depth, ALPHA, BETA, True, game, 1, WHITE)
+
             white_end_time = time.time()
             white_time = white_end_time - white_start_time
             game.ai_move(new_board)
@@ -43,7 +52,13 @@ def main():
             white_counter += 1
         elif game.turn == BLACK:
             black_start_time = time.time()
-            value, new_board = minimax(game.get_board(), depth, True, game, 2, BLACK)
+
+            # Minimax
+            # value, new_board = minimax(game.get_board(), depth, True, game, 1, BLACK)
+
+            # Alpha-Beta Pruning
+            value, new_board = alpha_beta_pruning(game.get_board(), depth, ALPHA, BETA, True, game, 2, BLACK)
+
             black_end_time = time.time()
             black_time = black_end_time - black_start_time
             game.ai_move(new_board)
@@ -51,14 +66,16 @@ def main():
             black_counter += 1
 
         if game.winner() is not None:
-            print('Winner is: ')
-            winner = game.winner()
-            if game.winner() == BLACK:
-                print('BLACK (AI)')
-            elif game.winner() == WHITE:
-                print('WHITE (AI)')
+            if game.winner() is not GREY:
+                print('Winner is: ')
+                if game.winner() == BLACK:
+                    print('BLACK (AI)')
+                elif game.winner() == WHITE:
+                    print('WHITE (AI)')
+                else:
+                    print(game.winner())
             else:
-                print(game.winner())
+                print('Draw')
             run = False
 
         for event in pygame.event.get():
