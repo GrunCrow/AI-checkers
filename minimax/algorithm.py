644d2d276ac58f95fa2d58214b289405c1a0e341
GrunCrow <albamrqz751@gmail.com>
2022-04-07 19:19:37 +0200

    Alpha-Beta Pruning has been implemented

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -20,7 +20,7 @@ def minimax(position, depth, max_player, game, evaluate_function=1, ai_colour=WH
     if max_player:  # max the score
         max_eval = float('-inf')     # init at -inf
         best_move = None
-        for move in get_all_moves(position, ai_colour, game):   # for every possible move
+        for move in get_all_moves(position, ai_colour):   # for every possible move
             evaluation = minimax(move, depth-1, False, game, evaluate_function, ai_colour)[0]    # next = min
             max_eval = max(max_eval, evaluation)
             if max_eval == evaluation:
@@ -30,7 +30,7 @@ def minimax(position, depth, max_player, game, evaluate_function=1, ai_colour=WH
     else:   # min the score
         min_eval = float('inf')  # init at inf for minimizing
         best_move = None
-        for move in get_all_moves(position, ai_colour, game):
+        for move in get_all_moves(position, ai_colour):
             evaluation = minimax(move, depth - 1, True, game, evaluate_function, ai_colour)[0]  # next = max
             min_eval = min(min_eval, evaluation)
             if min_eval == evaluation:
@@ -38,27 +38,27 @@ def minimax(position, depth, max_player, game, evaluate_function=1, ai_colour=WH
         return min_eval, best_move
 
 
-def alpha_beta_pruning(position, depth, max_player, game, evaluate_function=2):
-    #   position = board current position (origin)
-    #   depth = how depth is the tree going to be
-    #   max_player = if we are min or max the value (boolean)
-    #   game = game object
+def alpha_beta_pruning(position, depth, alpha, beta, max_player, game, evaluate_function=1, ai_colour=WHITE):
 
     #   evaluation starts at the bottom of the tree
     if depth == 0 or position.winner() is not None:
-        position_winner = position.winner()
-        evaluate = position.evaluate_function_1()
         if evaluate_function == 1:
-            return position.evaluate_function_1(), position
+            return position.evaluate_function_1(ai_colour), position
         elif evaluate_function == 2:
-            return position.evaluate_function_2(), position
+            return position.evaluate_function_2(ai_colour), position
 
     if max_player:  # max the score
         max_eval = float('-inf')     # init at -inf
         best_move = None
-        for move in get_all_moves(position, WHITE, game):   # for every possible move
-            evaluation = minimax(move, depth-1, False, game, evaluate_function)[0]    # next = min
+        for move in get_all_moves(position, ai_colour):   # for every possible move
+            evaluation = alpha_beta_pruning(move, depth-1, alpha, beta, False, game, evaluate_function, ai_colour)[0]
             max_eval = max(max_eval, evaluation)
+
+            # added code for alpha beta
+            alpha = max(alpha, evaluation)
+            if beta <= alpha:
+                break  # break the loop to stop analyzing
+
             if max_eval == evaluation:
                 best_move = move
         return max_eval, best_move
@@ -66,9 +66,15 @@ def alpha_beta_pruning(position, depth, max_player, game, evaluate_function=2):
     else:   # min the score
         min_eval = float('inf')  # init at inf for minimizing
         best_move = None
-        for move in get_all_moves(position, WHITE, game):
-            evaluation = minimax(move, depth - 1, True, game, evaluate_function)[0]  # next = max
+        for move in get_all_moves(position, ai_colour):
+            evaluation = alpha_beta_pruning(move, depth - 1, alpha, beta, True, game, evaluate_function, ai_colour)[0]
             min_eval = min(min_eval, evaluation)
+
+            # added code for alpha beta
+            beta = min(beta, evaluation)
+            if beta <= alpha:
+                break  # break the loop to stop analyzing
+
             if min_eval == evaluation:
                 best_move = move
         return min_eval, best_move
@@ -81,7 +87,7 @@ def simulate_move(piece, move, board, skip):
     return board
 
 
-def get_all_moves(board, colour, game):  # check all moves of all the pieces
+def get_all_moves(board, colour):  # check all moves of all the pieces
     moves = []
     for piece in board.get_all_pieces(colour):
         valid_moves = board.get_valid_moves(piece)
@@ -101,4 +107,3 @@ def draw_moves(game, board, piece):     # follow machine analysis
     game.draw_valid_moves((valid_moves.keys()))
     pygame.display.update()
     # pygame.time.delay(50)    # to show pieces analysis by machine slower - 100
-
